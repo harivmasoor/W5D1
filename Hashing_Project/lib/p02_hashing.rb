d9004d362761ce4448ceaf58680d034c8ffa307e
harivmasoor <harimasoor@gmail.com>
2023-06-19 16:35:43 -0700

    hashes

--- Hashing_Project/lib/p02_hashing.rb
+++ Hashing_Project/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    arr = []
+    self.each_with_index do |el, i|
+      arr << (el.hash*i)
+    end
+    return arr.sum
   end
 end
 
 class String
   def hash
+    num = 0
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    self.each_char.with_index do |char, i|
+      num += char.ord.hash * i * alphabet.index(char.downcase)
+    end
+    num
   end
 end
 
@@ -16,6 +27,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    array = []
+    self.each do |k,v|
+      array << v.ord.hash
+    end
+    array.sum
   end
 end
