8cfe4f926918598f69b9bbdaab79ba471c08243a
Weyman Leung <leungweyman@gmail.com>
2023-06-19 16:58:06 -0700

    finish hash set

--- Hashing_Project/lib/p03_hash_set.rb
+++ Hashing_Project/lib/p03_hash_set.rb
@@ -7,12 +7,26 @@ class HashSet
   end
 
   def insert(key)
+	if !include?(key)
+		@store[key.hash % num_buckets] << key
+		@count += 1
+	end
+	resize! if @count == num_buckets
   end
 
   def include?(key)
+	@store.each do |bucket|
+		return true if bucket.include?(key)
+	end
+	false
   end
 
   def remove(key)
+	if include?(key)
+		index = @store[key.hash % num_buckets].index(key) 
+		@store[key.hash % num_buckets].delete_at(index)
+		@count -= 1 
+	end
   end
 
   private
@@ -22,6 +36,18 @@ class HashSet
   end
 
   def resize!
+	if num_buckets == @count
+		prev_elements = []
+		@store.each do |bucket|
+			prev_elements += bucket
+		end
+		
+		@store = Array.new(num_buckets * 2) { Array.new }
+
+		prev_elements.each do |num|
+			@store[num % (num_buckets * 2)] << num
+		end
+	end
   end
 
   def [](num)
