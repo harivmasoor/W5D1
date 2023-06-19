f6bc1af8934fc1f34b176e01a53797e93da942b2
Weyman Leung <leungweyman@gmail.com>
2023-06-19 15:20:41 -0700

    finish int sets

--- Hashing_Project/lib/p01_int_set.rb
+++ Hashing_Project/lib/p01_int_set.rb
@@ -64,12 +64,26 @@ class ResizingIntSet
   end
 
   def insert(num)
+	if !include?(num) && @count < num_buckets
+		@store[num % num_buckets] << num 
+		@count += 1
+	end
+	resize! if @count == num_buckets
   end
 
   def remove(num)
+	if include?(num)
+		index = @store[num % num_buckets].index(num)
+    	@store[num % num_buckets].delete_at(index)
+		@count -= 1
+	end
   end
 
   def include?(num)
+	@store.each do |bucket|
+		return true if bucket.include?(num)
+	end
+	false
   end
 
   private
@@ -79,6 +93,18 @@ class ResizingIntSet
   end
 
   def resize!
+	if num_buckets == @count
+		prev_elements = []
+		@store.each do |bucket|
+			prev_elements += bucket
+		end
+		
+		@store = Array.new(num_buckets * 2) { Array.new }
+
+		prev_elements.each do |num|
+			@store[num % (num_buckets * 2)] << num
+		end
+	end
   end
 
   def [](num)
